78a4e8cd8158eeef84776e0817e21b20327bd1e5
hyeondeok <nicknamem7@gmail.com>
2019-09-03 12:11:52 +0900

    전역예외처리 구현

---
+++ src/main/java/board/common/ExceptionHandler.java
@@ -0,0 +1,22 @@
+package board.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice //예외처리 클래스임을 나타내는 어노테이션
+@Slf4j
+public class ExceptionHandler {
+	
+	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class) //실제 프로젝트에서는 금물, 일단 모든 예외를 처리하도록 설정
+	public ModelAndView defaultExceptionHandler(HttpServletRequest request, Exception exception){
+		ModelAndView mv = new ModelAndView("/error/error_default"); //예외 발생 시 보여줄 화면
+		mv.addObject("exception", exception);
+		
+		log.error("defaultExceptionHandler", exception);
+		
+		return mv;
+	}
+}
